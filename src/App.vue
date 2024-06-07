2992a605bd7fc7a8ef49688baded1f4e0a15f9b2
AlesciDavide <davi.alesci@email.com>
2024-06-07 12:58:29 +0200

    imprtazione componente

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
-<script setup>
-import HelloWorld from './components/AppTitle.vue'
+<script >
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  components:{
+    AppTitle,
+  }
+}
 </script>
 
 <template>
-  
+    <AppTitle />
 </template>
 
 <style scoped>
